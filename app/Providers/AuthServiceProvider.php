742e204020068c406c60ba5b23b8339eea290c7d
BigPang <yue26xin@126.com>
2016-11-11 11:15:47 +0800

    权限与迁移搞定

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Http\Requests\Request;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use League\Flysystem\Exception;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,13 +28,18 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(GateContract $gate)
     {
+        if(!empty($_SERVER['SCRIPT_NAME']) && strtolower($_SERVER['SCRIPT_NAME']) ==='artisan' ){
+            return false;
+        }
         $gate->before(function ($user, $ability) {
             if ($user->id === 1) {
                 return true;
             }
         });
         $this->registerPolicies($gate);
-        $permissions = \App\Models\Admin\Permission::with('roles')->get();
+
+            $permissions = \App\Models\Admin\Permission::with('roles')->get();
+
         foreach ($permissions as $permission) {
             $gate->define($permission->name, function ($user) use ($permission) {
                 return $user->hasPermission($permission);
