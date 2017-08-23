3187337406ba647ee9f6aadabaffe97a8b22c399
BigPang <yue26xin@126.com>
2017-08-23 10:01:51 +0800

    修改组错误

--- resources/views/admin/layouts/mainFooter.blade.php
+++ resources/views/admin/layouts/mainFooter.blade.php
@@ -4,5 +4,5 @@
       Designed by AdminLTE
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="#">Zuosai</a>.</strong> All rights reserved.
+      <strong>Copyright &copy; 2017 <a href="https://github.com/big-pang">BigPang</a>.</strong>
   </footer>
